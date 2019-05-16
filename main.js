e128babdab6de35e177726fb6442363a04fd8e14
akjeres <akjeres@gmail.com>
2019-05-16 19:06:25 +0300

    Vars changed to Lets

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
    window.addEventListener('load', function(e) {
-        var c1 = new Concatenator({
+        let c1 = new Concatenator({
             target:document.getElementById('root'),
         });
         c1.render();
@@ -9,7 +9,7 @@
 })();
 
 function Concatenator(options) {
-    var defaults = {
+    let defaults = {
         target: document.getElementsByTagName('body')[0],
         emptyLabelValue: 'No image selected',
         width: '800px',
@@ -17,24 +17,24 @@ function Concatenator(options) {
         verticalMode: false,
         statusBar: true,
     };
-    var opts = Object.assign(Object.create(defaults), options);
-    var _button = document.createElement('button');
-    var _form =  document.createElement('form');
-    var _canvas = document.createElement('div'); //change to "canvas"
-    var _concatenator = createConcatenator();
+    let opts = Object.assign(Object.create(defaults), options);
+    let _button = document.createElement('button');
+    let _form =  document.createElement('form');
+    let _canvas = document.createElement('div'); //change to "canvas"
+    let _concatenator = createConcatenator();
     _concatenator.style.width = (parseInt(opts.width) + 6) + 'px';
-    var _target = opts.target;
-    var self = this;
+    let _target = opts.target;
+    let self = this;
     function createConcatenator() {
-        var elem = document.createElement('div');
+        let elem = document.createElement('div');
         elem.setAttribute('class', 'concatenator');
         //Canvas attributes
         _canvas.setAttribute('class', 'canvas');
         _canvas.style.width = opts.width;
         _canvas.style.height = parseInt(parseInt(opts.width) * 0.75) + 'px';
         //Management bar
-        var m = createEl('div', 'management');
-        var cB = createEl('input', 'checkbox');
+        let m = createEl('div', 'management');
+        let cB = createEl('input', 'checkbox');
         cB.setAttribute('type', 'checkbox');
         cB.setAttribute('name', 'verticalMode');
         cB.setAttribute('tabindex', '0');
@@ -47,11 +47,11 @@ function Concatenator(options) {
         addField();addField();
         //Status bar (optional)
         if (opts.statusBar) {
-            var bar = createEl('div', 'status-bar');
-            var btns = createEl('div', 'buttons');
-            var bG = createEl('div', 'button button-green');
-            var bY = createEl('div', 'button button-yellow');
-            var bR = createEl('div', 'button button-red');
+            let bar = createEl('div', 'status-bar');
+            let btns = createEl('div', 'buttons');
+            let bG = createEl('div', 'button button-green');
+            let bY = createEl('div', 'button button-yellow');
+            let bR = createEl('div', 'button button-red');
             btns.appendChild(bG);
             btns.appendChild(bY);
             btns.appendChild(bR);
@@ -66,7 +66,7 @@ function Concatenator(options) {
         _target.appendChild(_concatenator);
     }
     function addField() {
-        var label, input, span;
+        let label, input, span;
         label = createEl('label', 'input-label');
         label.setAttribute('tabindex', '0');
         input = createEl('input', 'input');
@@ -78,17 +78,17 @@ function Concatenator(options) {
         label.appendChild(input);
         label.appendChild(span);
         if (_form.elements.length > 3) { //Disable adding remove spans to two first inputs type file
-            var close = createEl('span', 'remove-input');
+            let close = createEl('span', 'remove-input');
             close.textContent = '×';
             label.appendChild(close);
         }
         _form.appendChild(label);
     }
     function getFormValues(form) {
-        var elements = form.elements;
-        var verticalMode = false;
-        var result = [];
-        var divider = '-';
+        let elements = form.elements;
+        let verticalMode = false;
+        let result = [];
+        let divider = '-';
         Array.prototype.map.call(elements, function(i) {
             if (i.type != 'checkbox' && validateType(i.value)) {
                 result.push(i.value);
@@ -101,23 +101,23 @@ function Concatenator(options) {
     function validateType(value) {
         return /\.(jpe?g|png|gif)$/i.test(value);
     }
-    function setLabelValue(input) {
-        var value = opts.emptyLabelValue;
-        if (input && input.value) {
-            value = input.value.replace(/^.+(\\|\/)/ig, '');
+    function setLabelValue(inp) {
+        let value = opts.emptyLabelValue;
+        if (inp && inp.value) {
+            value = inp.value.replace(/^.+(\\|\/)/ig, '');
         }
-        input.nextElementSibling.textContent = value;
+        inp.nextElementSibling.textContent = value;
     }
     function renderResult() {
         _canvas.innerHTML = getFormValues(_form);
     }
     function createEl(tag_name, class_name) {
-        var res = document.createElement(tag_name);
+        let res = document.createElement(tag_name);
         res.setAttribute('class', class_name);
         return res;
     }
-    function removeField(input) {
-        var input = arguments[0]; //event.target
+    function removeField() {
+        let input = arguments[0]; //event.target
         if (_form.elements.length > 4) { //Disable removing two first inputs type file
             if (arguments && arguments.length) {
                 input.tagName.toLowerCase() == 'span' && input.parentNode.remove();
@@ -128,7 +128,7 @@ function Concatenator(options) {
     }
     //Event listeners
     _form.addEventListener('click', function(e) {
-        var target = e.target;
+        let target = e.target;
         if (target) {
             if (target.classList.contains('remove-input')) {
                 e.preventDefault();
@@ -145,11 +145,11 @@ function Concatenator(options) {
         }
     });
     _form.addEventListener('input', function(e) {
-        var target = e.target;
+        let target = e.target;
         if (target && target.type == 'file' && validateType(target.value)) {
             if (!validateType(target.value)) target.value = '';
         }
-        for (var key in this.elements) {
+        for (let key in this.elements) {
             if (this.elements[key]['type'] == 'file') setLabelValue(this.elements[key]);
         }
         self.renderResult();
